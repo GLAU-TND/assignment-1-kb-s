f4bb9ff90bb26c86b8e075c80a34f8c66b1e0cec
kb-s <kanhaiya.bhatt_cs18@gla.ac.in>
2020-02-22 12:27:48 +0530

    create a Node class with the help of generics concept

--- src/definition/List.java
+++ src/definition/List.java
@@ -1,4 +1,28 @@
 package definition;
 
-public class List {
+import adt.ListADT;
+
+public class List<E> implements ListADT<E> {
+
+    @Override
+    public boolean add(E item) {
+        return false;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public int search(E data) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+
 }
